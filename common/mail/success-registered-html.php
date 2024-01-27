c5b7a06bc65f873de5481243218d9682524b383d
sarvar <sarvar.nishonboyev.external@raisin.com>
2024-01-27 14:43:37 +0500

    improve config

--- common/mail/success-registered-html.php
+++ common/mail/success-registered-html.php
@@ -1,4 +1,5 @@
 <?php
+
 use common\components\App;
 
 /** @var $voter \common\models\Voter */
@@ -7,10 +8,10 @@ use common\components\App;
 
 <div>
     <p></p>
-    <p><?=Yii::t('main','You have successfully registered for Voting application of EaP Civil Society Hackathon. ',['appName' => Yii::$app->name])?></p>
-    <p><?=Yii::t('main','Your login is : {login}',['login' => $voter->username])?></p>
-    <p><?=Yii::t('main','Your password is : {password}',['password' => $voter->password])?></p>
-    <p><?=Yii::t('main','URL to system: <a href="{url}">{url}</a>',['url' => App::getFrontend()])?></p>
+    <p><?= Yii::t('main', 'You have successfully registered for Voting application of EaP Civil Society Hackathon. ', ['appName' => Yii::$app->name]) ?></p>
+    <p><?= Yii::t('main', 'Your login is : {login}', ['login' => $voter->username]) ?></p>
+    <p><?= Yii::t('main', 'Your password is : {password}', ['password' => $voter->password]) ?></p>
+    <p><?= Yii::t('main', 'URL to the system: <a href="{url}">{url}</a>', ['url' => App::getFrontend()]) ?></p>
     <br>
-    <p><?=Yii::t('main','Thank you!')?></p>
+    <p><?= Yii::t('main', 'Thank you!') ?></p>
 </div>
